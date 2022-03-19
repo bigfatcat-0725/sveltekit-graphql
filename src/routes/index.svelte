07ab356b444593802c908e181bcacf27308d7035
bigfatcat <bigfatcat0725@gmail.com>
2022-03-20 04:36:31 +0900

    first commit

---
+++ src/routes/index.svelte
@@ -0,0 +1,31 @@
+<script context="module">
+	import { request, gql } from 'graphql-request';
+
+	const query = gql`
+		{
+			countries {
+				name
+				emoji
+			}
+		}
+	`;
+
+	export const load = async () => {
+		const data = await request('https://countries.trevorblades.com/', query);
+
+		return {
+			props: {
+				data
+			}
+		};
+	};
+</script>
+
+<script>
+	export let data;
+</script>
+
+<h1>Countries</h1>
+{#each data.countries as { name, emoji }}
+	<p>{emoji} {name}</p>
+{/each}
